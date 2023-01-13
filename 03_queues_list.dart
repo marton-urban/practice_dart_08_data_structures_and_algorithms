ba7aee760186134176c5c4ff31dff3f1a1c1859c
Marton Urban <urban.marton@gmail.com>
2023-01-13 21:32:59 +0100

    add new file

---
+++ 03_queues_list.dart
@@ -0,0 +1,42 @@
+abstract class Queue<E> {
+  bool enqueue(E element);
+  E? dequeue();
+  bool get isEmpty;
+  E? get peek;
+}
+
+class QueueList<E> implements Queue<E> {
+  final _list = <E>[];
+
+  @override
+  bool enqueue(E element) {
+    _list.add(element);
+    return true;
+  }
+
+  @override
+  E? dequeue() => (isEmpty) ? null : _list.removeAt(0);
+
+  @override
+  bool get isEmpty => _list.isEmpty;
+
+  @override
+  E? get peek => (isEmpty) ? null : _list.first;
+
+  @override
+  String toString() => _list.toString();
+}
+
+void main() {
+  final queue = QueueList<String>();
+  queue.enqueue('Ray');
+  queue.enqueue('Brian');
+  queue.enqueue('Eric');
+  print(queue);
+
+  queue.dequeue();
+  print(queue);
+
+  queue.peek;
+  print(queue);
+}
