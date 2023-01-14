893a7818b1f224c2c8cc19dd83aaa4a4e840733f
Marton Urban <urban.marton@gmail.com>
2023-01-14 17:18:14 +0100

    add challengeFour

--- 05_queues_doubly_linked_list.dart
+++ 05_queues_doubly_linked_list.dart
@@ -184,6 +184,85 @@ void enqueueDequeueExample() {
   print(queue);
 }
 
+void challengeFour() {
+  final deque = DequeDoublyLinkedList<int>();
+  deque.enqueue(1, Direction.back);
+  deque.enqueue(2, Direction.back);
+  deque.enqueue(3, Direction.back);
+  deque.enqueue(4, Direction.back);
+
+  print(deque);
+
+  deque.enqueue(5, Direction.front);
+
+  print(deque);
+
+  deque.dequeue(Direction.back);
+  deque.dequeue(Direction.back);
+  deque.dequeue(Direction.back);
+  deque.dequeue(Direction.front);
+  deque.dequeue(Direction.front);
+  deque.dequeue(Direction.front);
+
+  print(deque);
+}
+
+enum Direction {
+  front,
+  back,
+}
+
+abstract class Deque<E> {
+  bool get isEmpty;
+  E? peek(Direction from);
+  bool enqueue(E element, Direction to);
+  E? dequeue(Direction from);
+}
+
+class DequeDoublyLinkedList<E> implements Deque<E> {
+  final _list = DoublyLinkedList<E>();
+
+  @override
+  E? dequeue(Direction from) {
+    switch (from) {
+      case Direction.front:
+        return _list.pop();
+      case Direction.back:
+        return _list.removeLast();
+    }
+  }
+
+  @override
+  bool enqueue(E value, Direction to) {
+    switch (to) {
+      case Direction.front:
+        _list.push(value);
+        break;
+      case Direction.back:
+        _list.append(value);
+        break;
+    }
+    return true;
+  }
+
+  @override
+  bool get isEmpty => _list.isEmpty;
+
+  @override
+  E? peek(Direction from) {
+    switch (from) {
+      case Direction.front:
+        return _list.head?.value;
+      case Direction.back:
+        return _list.tail?.value;
+    }
+  }
+
+  @override
+  String toString() => _list.toString();
+}
+
 void main() {
-  enqueueDequeueExample();
+  // enqueueDequeueExample();
+  challengeFour();
 }
